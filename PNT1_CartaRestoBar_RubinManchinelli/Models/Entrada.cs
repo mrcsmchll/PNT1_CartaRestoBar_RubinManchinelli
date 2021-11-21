5da16877cf1a487d027d03d49f7fad89c4e86af4
Marcos Manchinelli <37360946@comunidadort.com.ar>
2021-11-21 19:56:18 -0300

    Agrega clases Entrada Plato y Postre

---
+++ PNT1_CartaRestoBar_RubinManchinelli/Models/Entrada.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PNT1_CartaRestoBar_RubinManchinelli.Models
+{
+      public class Entrada : Plato
+        {
+          public Entrada(int id, string nombre, double precio, string descripcion, bool esAptoCeliaco, bool esAptoVegano) : base(id, nombre, precio, descripcion, esAptoCeliaco, esAptoVegano)
+            {
+
+            }
+        }
+    }
+}
---
+++ PNT1_CartaRestoBar_RubinManchinelli/Models/Plato.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PNT1_CartaRestoBar_RubinManchinelli.Models
+{
+    public class Plato
+    {
+        public Plato(int id, string nombre, double precio, string descripcion, bool esAptoCeliaco, bool esAptoVegano)
+        {
+            this.Id = id;
+            this.Nombre = nombre;
+            this.Precio = precio;
+            this.Descripcion = descripcion;
+            this.esAptoCeliaco = esAptoCeliaco;
+            this.esAptoVegano = esAptoVegano;
+        }
+
+        private int Id;
+        private string Nombre;
+        private double Precio;
+        private string Descripcion;
+        private bool esAptoCeliaco;
+        private bool esAptoVegano;
+
+    }
+
+}
+
