2a53148970fd19e918c57ef325da996f5cba9b4f
Marcos Manchinelli <37360946@comunidadort.com.ar>
2021-11-21 21:28:25 -0300

    Cambios en startup.cs

--- PNT1_CartaRestoBar_RubinManchinelli/Startup.cs
+++ PNT1_CartaRestoBar_RubinManchinelli/Startup.cs
@@ -1,13 +1,18 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using PNT1_CartaRestoBar_RubinManchinelli.Context;
+using Newtonsoft.Json;
 
 namespace PNT1_CartaRestoBar_RubinManchinelli
 {
