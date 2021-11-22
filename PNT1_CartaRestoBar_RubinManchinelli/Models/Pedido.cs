17b681b36f9e4b46d5bb6192f1b4efc876cb61db
Marcos Manchinelli <37360946@comunidadort.com.ar>
2021-11-22 16:36:54 -0300

    //

--- PNT1_CartaRestoBar_RubinManchinelli/Models/Pedido.cs
+++ PNT1_CartaRestoBar_RubinManchinelli/Models/Pedido.cs
@@ -8,21 +8,35 @@ namespace PNT1_CartaRestoBar_RubinManchinelli.Models
 {
     public class Pedido
     {
-        public Pedido(){}
-        public Pedido(int id, Cliente c)
+        public Pedido() { }
+        public Pedido(int id, Cliente c, TipoPlato tp, TipoPlato tipoPlatos)
         {
             this.Id = id;
             this.Cliente = c;
             this.platos = new List<Plato>();
-
+            TipoPlatos = tipoPlatos;
+         
+            cargarPlato();
         }
 
-        
+        public void cargarPlato()
+        {
+            platos.Add(new Plato(1, "Pizza", 250, "", false, false));
+            platos.Add(new Plato(2, "Pasta", 250, "", false, true));
+            platos.Add(new Plato(3, "Carne", 800, "", false, false));
+            platos.Add(new Plato(4, "Pescado", 1000, "", false, false));
+        }
+
+
         public int Id { get; set; }
         public Cliente Cliente { get; set; }
 
+        [EnumDataType(typeof(TipoPlato))]
+        public TipoPlato TipoPlatos;
+
         public List<Plato> platos;
 
+      
     }
 
 
---
+++ PNT1_CartaRestoBar_RubinManchinelli/Models/TipoPlato.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PNT1_CartaRestoBar_RubinManchinelli.Models
+{
+    public enum TipoPlato
+    {
+        Pizza,
+        Pasta,
+        Carne,
+        Pescado,
+        Frutas
+
+    }
+}
